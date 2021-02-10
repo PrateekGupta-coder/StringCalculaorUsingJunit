c08cf8bf3b273dc7233a226b6d591309cc28732d
PrateekGupta-coder <78865651+PrateekGupta-coder@users.noreply.github.com>
2021-02-10 21:25:46 +0530

    Update Calculator.java
    Sum of two numbers

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,11 @@ public class Calculator {
 	public static int add(String text) {
 		if(text.isEmpty()) {
 			return 0;
-		}else {
+		}else if(text.contains(",")) {
+			String str[] = text.split(",");
+			return Integer.parseInt(str[0])+Integer.parseInt(str[1]);
+		}
+		else {
 			return Integer.parseInt(text);
 		}
 	}
