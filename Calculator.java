b910cda023476e609b8df1a7afae32432dcd0ab7
PrateekGupta-coder <78865651+PrateekGupta-coder@users.noreply.github.com>
2021-02-10 22:03:23 +0530

    Update Calculator.java
    For Multiple numbers

--- Calculator.java
+++ Calculator.java
@@ -3,15 +3,29 @@ package calculator;
 public class Calculator {
 
 	public static int add(String text) {
+		List<Integer>  list = new ArrayList<Integer>();
 		if(text.isEmpty()) {
 			return 0;
 		}else if(text.contains(",")) {
 			String str[] = text.split(",");
-			return Integer.parseInt(str[0])+Integer.parseInt(str[1]);
+			for (String sText : str) {
+				list.add(convertTo(sText));
+			}
+			return sumOfElements(list);
 		}
 		else {
-			return Integer.parseInt(text);
+			list.add(convertTo(text));
+			return sumOfElements(list);
 		}
+	}
+	
+	private static Integer convertTo(String text) {
+		return Integer.parseInt(text);
+	}
+	
+	private static Integer sumOfElements(List<Integer> list) {
+		Integer sum = list.stream().mapToInt(i -> i).sum();
+		return sum;
 	}
 
 }
